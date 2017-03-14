c4f8d1cbd513778817bac0cd174640d582c7e77e
Christoph-Bolz <chriss.bolz22@gmail.com>
2017-03-14 19:06:56 +0100

    New Java Project

---
+++ 226GIT/src/ch/csbe/git/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ch.csbe.git;
+
+/**
+ * @author vmadmin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
